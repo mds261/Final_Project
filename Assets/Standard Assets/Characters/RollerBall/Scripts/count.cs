e8554c2a9e4680ece236730cef16df577260b48d
Matthew Schmidt <mds88@live.com>
2018-07-19 18:55:52 -0400

    Final Project
    A race against the clock to escape an Island being rained on by meteors.

---
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/count.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class count : MonoBehaviour
+{
+    public float resetDelay;
+    float timeRemaining = 300.0f;
+
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+    }
+
+    void OnGUI()
+    {
+        if (timeRemaining > 0)
+        {
+            GUI.Label(new Rect(100, 100, 200, 100),
+                         "Time Remaining : " + timeRemaining);
+        }
+        else
+        {
+
+            GUI.Label(new Rect(100, 100, 200, 100), "You Failed to Escape!");
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+         
+        
+    }
+}
+
+
+    
+
+    
+
+        
+    
+    
---
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/gamemng.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemng : MonoBehaviour {
+
+    public static gamemng instance = null;
+
+    public GameObject YouWinText;
+    public float resetDelay;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+    public void Win()
+    {
+        YouWinText.SetActive(true);
+        Time.timeScale = .25f;
+        Invoke("Reset", resetDelay);
+    }
+
+    void Reset()
+    {
+        Time.timeScale = 1.0f;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    
+}
